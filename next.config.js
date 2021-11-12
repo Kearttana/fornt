e3a6031831d1784936f55c07a13bba498accfbe3
kearttana <kearttana.sri@spumail.net>
2021-11-13 02:30:42 +0700

    fixed

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-
-const withSass = require('@zeit/next-sass');
-const withImages = require('next-images');
-const withLess = require('@zeit/next-less')
-const withCSS = require('@zeit/next-css')
-
-module.exports = withCSS(withLess(withImages(withSass({
-  env: {
-    
-  }
-}))));
+module.exports = {
+  reactStrictMode: true,
+}
